ebed36ce1a620b758c8f6a8414e143dc1d3a51f9
Syed Fazil <syedfazil539@gmail.com>
2026-08-16 22:25:48 +0530

    feat(setup): added postgres db config and compose

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,9 @@
 package config
 
 type Config struct {
-	Logging LoggingConfig `yaml:"logging"`
-	Bifrost BifrostConfig `yaml:"bifrost"`
+	Logging  LoggingConfig  `yaml:"logging"`
+	Bifrost  BifrostConfig  `yaml:"bifrost"`
+	Database DatabaseConfig `yaml:"database"`
 }
 
 type LoggingConfig struct {
@@ -11,4 +12,13 @@ type LoggingConfig struct {
 
 type BifrostConfig struct {
 	Name string `yaml:"name"`
+}
+
+type DatabaseConfig struct {
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	Name     string `yaml:"name"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	SSLMode  string `yaml:"sslmode"`
 }
